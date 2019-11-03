5da106447dde1cf328d2809a0bcf3990002bd859
Hugo Rabock <hugooliv@hotmail.com>
2019-11-03 19:45:42 -0300

    realizando correcoes

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,9 @@ const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
 const multer = require('multer');
 const uploadConfig  = require('./config/upload');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
+
 const upload  = multer(uploadConfig);
 
 const routes = express.Router();
@@ -10,5 +13,11 @@ const routes = express.Router();
 routes.post('/users', SessionController.store);
 
 routes.post('/spots', upload.single('thumbnail'),SpotController.store);
+routes.post('/spots/:spot_id/bookings',BookingController.store);
+
+routes.get('/spots',SpotController.index);
+
+routes.get('/dashboard',DashboardController.index);
+
 
 module.exports = routes;
--- src/controllers/SpotController.js
+++ src/controllers/SpotController.js
@@ -2,13 +2,20 @@ const Spot = require('../models/Spot')
 const User = require('../models/User')
 
     module.exports = {
+        async index(req, res){
+            const {tech} = req.query;
+            const spot = await Spot.find({techs : tech})
+            return res.json(spot);
+        },
+        
         async store(req, res){
             const {filename} = req.file;
             const {company, price, techs} = req.body;
             const {user_id} = req.headers;
             const user = await User.findById(user_id); 
             if (!user)
-                res.status(404).json({error : 'Usuario não existe'});
+               return res.status(404).json({error : 'Usuario não existe'});
+
             let spot = await Spot.create({
                 company: company,
                 user : user_id,
